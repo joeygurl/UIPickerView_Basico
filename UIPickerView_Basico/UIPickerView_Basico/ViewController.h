b6d4a47e97c4046ad68716e4e6b21b84f468ede6
Henry AT <yiperu@gmail.com>
2013-09-30 11:51:21 -0500

    Inicializamos el proyecto mediante un aplicacion simple

---
+++ UIPickerView_Basico/UIPickerView_Basico/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIPickerView_Basico
+//
+//  Created by Henry AT on 9/30/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
